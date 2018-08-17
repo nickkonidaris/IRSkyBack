772b13539319bcc2223f22799813e37ebb6dd4cb
Nick Konidaris <nick.konidaris@gmail.com>
2018-08-17 14:14:46 -0700

    created

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+
+setup(
+    name = "",
+    version = "0.0.1",
+    author = "Nicholas P. Konidaris",
+    author_email = "npk@carnegiescience.edu",
+    description = ("Swope data reduction"),
+    license = "GNU General Public License v3.0",
+    keywords = "Python",
+    url = "https://github.com/nickkonidaris/Swope-reduction",
+    packages=[''],
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python',
+    ],
+)
